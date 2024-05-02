0719665220b08ce22a4bbda8a1f18734c5848bbd
ylvz <142927874+ylvz@users.noreply.github.com>
2024-05-02 12:01:40 +0200

    hh

--- BankAccount2/Accunt.cs
+++ BankAccount2/Accunt.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BankAccount2
+namespace Part2
 {
-    internal class NewBankAccount
+    internal class Accunt
     {
+        int balance;
+        public Mutex transferMutex = new Mutex();
+
+        public void NoDeadLockTransfer(Mutex mutexLock, Accunt reciever,int amount, string account,string recieverName)
+        {
+            mutexLock.WaitOne();
+            Thread.Sleep(500);
+            reciever.transferMutex.WaitOne();
+            reciever.balance -= amount;
+            Console.WriteLine(account + " is inside the transfer method");
+            Console.WriteLine($"{amount} was transferred to {recieverName} from {account}. {account} " + $"Current balance = {balance}");
+            mutexLock.ReleaseMutex();
+            reciever.transferMutex.ReleaseMutex();
+        }
+
+        public void DeadlockTransfer(Accunt reciever, int amount, string account,string recieverName) 
+        {
+            lock (this)
+            {
+                Thread.Sleep(50);
+                lock (reciever)
+                {
+                    reciever.balance -= amount;
+                    balance -= amount;
+                    Console.WriteLine();
+                    Console.WriteLine();
+                }
+            }
+        }
     }
 }
--- BankAccount2/Transaction.cs
+++ BankAccount2/Transaction.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace Part2
 {
+
     internal class Transaction
     {
+        Accunt account1 = new Accunt();
+        Accunt account2 = new Accunt();
+        Mutex mutexLock=new Mutex();
+        bool isRunning = true;
+
+        public void StartDeadlockTransaction() 
+        {
+            Thread t1 = new Thread(() => account1.DeadlockTransfer(account2, 500, "Account1", "Account2"));
+            Thread t2 = new Thread(() => account2.DeadlockTransfer(account1, 500, "Account2", "Account1"));
+
+            t1.Start();
+            t2.Start();
+
+        }
+
+        public void StartNoDeadlockTransaction() 
+        {
+            Thread t3 = new Thread(() => account1.NoDeadLockTransfer(mutexLock, account2, 500, "Account1", "Account2"));
+            Thread t4 = new Thread(() => account2.NoDeadLockTransfer(mutexLock, account1, 500, "Account2", "Account1"));
+            t3.Start();
+            t4.Start();
+        }
+
+        public bool IsRunning { get { return isRunning; } set { isRunning = value; } }
+
     }
 }
