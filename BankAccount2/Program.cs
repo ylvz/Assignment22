4c83684914d89fc092ff4a79e62af1038ae37d34
ylvz <142927874+ylvz@users.noreply.github.com>
2024-05-02 12:22:45 +0200

    KLART

--- BankAccount2/Program.cs
+++ BankAccount2/Program.cs
@@ -2,9 +2,33 @@
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
-            
+            Transaction transaction = new Transaction();
+            bool validInput = false;
+
+            while(!validInput)
+            {
+                Console.WriteLine("Press 1 for failing to lock or 2 successful locking.");
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        transaction.StartDeadlockTransaction();
+                        validInput = true;
+                        break;
+                    case "2":
+                        transaction.StartNoDeadlockTransaction();
+                        validInput = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input, enter one of the two options.");
+                        break;
+                }
+            }
+
         }
     }
 }
